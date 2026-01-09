871c2717b2b1e3605f3a0c1b33fe25cf87b6c16b
VIVEK KODAVATI <vivekkodavati05@gmail.com>
2026-01-09 11:32:17 +0530

    EXP-01 Linear Regression, default, test_split-0.2, no preprocessing

--- train.py
+++ train.py
@@ -23,10 +23,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Preprocessing
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+
 
 # Model
 model = LinearRegression()
